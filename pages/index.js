ab585c5693719538eff92f7ab960a6909785a1c7
PRbsas <paularamirez0316@gmail.com>
2018-06-07 13:17:29 -0400

    Navigation between Pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link'
+
 const Index = () => (
   <div>
+    <Link href='about'>
+      <a style={{ fontSize: 20 }}>About Page</a>
+    </Link>
+    <Link href='about'>
+      <button>Go to About Page</button>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 )
